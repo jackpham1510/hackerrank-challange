4e96418526693c5bc323529e66ba9f9858dcf41d
Pham Quan Tien Dung <dungpqt@vng.com.vn>
2020-02-26 23:48:15 +0700

    update

--- hackerrank_challenge_server/server.js
+++ hackerrank_challenge_server/server.js
@@ -26,12 +26,6 @@ async function startServer() {
     console.error(err);
     res.sendStatus(err.status || 500);
   });
-
-  if (process.env.NODE_ENV !== 'dev') {
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname + '/public/index.html'));
-    });
-  }
   
   const PORT = process.env.PORT || 8133;
   
